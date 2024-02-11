06d92612ae7c97843d5f6a74bab1386f0c955a77
Aman Danidhariya <amandanidhariya007@gmail.com>
2024-02-11 18:36:11 +0530

    adding a NoProjectSelected component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,17 @@
 const Input = ({ label, textarea, ...props }) => {
+  const inputClass =
+    "w-full p-1 border-b-2 rounded-md bg-stone-200 text-stone-600  focus:outline-none focus:border-stone-600";
+
   return (
-    <p>
-      <label>{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea className={inputClass} {...props} />
+      ) : (
+        <input className={inputClass} {...props} />
+      )}
     </p>
   );
 };
