b7408e972d9504e4e4ede25cb1233ff36a9df414
Aman Danidhariya <amandanidhariya007@gmail.com>
2024-07-10 09:35:18 +0530

    build project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = ({
       <ul className="mt-8">
         {projects?.map((project) => {
           let cssClasses =
-            "w-full text-left px-2 py-1 rounded-sm my-1  hover:text-stone-200 hover:bg-stone-800";
+            "w-full text-xl text-left px-2 py-1 rounded-sm my-1 hover:bg-stone-800 hover:text-stone-200";
 
           if (project.id === selectedProjectId) {
             cssClasses += "bg-stone-800 text-stone-200";
